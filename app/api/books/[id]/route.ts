ae0b8985bf3149019db4ff1e6b8a6283457787b0
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2025-09-04 20:04:50 +0500

    resolved promise error

--- app/api/books/[id]/route.ts
+++ app/api/books/[id]/route.ts
@@ -2,65 +2,52 @@ import { NextRequest, NextResponse } from "next/server"
 import { StatusCodes } from "http-status-codes"
 import { prisma } from "@/app/utils/prisma"
 
-
-
-
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-
-    // console.log(id)
-
+export async function GET(
+    req: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
     try {
-
-        const id = Number(params.id)
-
+        const resolvedParams = await params
+        const id = Number(resolvedParams.id)
         const bookbyid = await prisma.book.findUnique({
             where: {
                 id
             }
         })
+
         return NextResponse.json({
             message: "Success",
             data: bookbyid
         }, { status: StatusCodes.OK })
 
-
-
     } catch (error) {
         return NextResponse.json({
-            message: "Error Occured"
+            message: "Error Occurred"
         }, { status: StatusCodes.INTERNAL_SERVER_ERROR })
     }
-
-
-
 }
 
-
-
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-
+export async function DELETE(
+    req: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
     try {
-
-        const id = Number(params.id)
+        const resolvedParams = await params
+        const id = Number(resolvedParams.id)
 
         await prisma.book.delete({
             where: {
                 id
             }
         })
+
         return NextResponse.json({
             message: "Successfully Deleted",
         }, { status: StatusCodes.OK })
 
-
-
     } catch (error) {
         return NextResponse.json({
-            message: "Error Occured"
+            message: "Error Occurred"
         }, { status: StatusCodes.INTERNAL_SERVER_ERROR })
     }
-
-
-
 }
-
